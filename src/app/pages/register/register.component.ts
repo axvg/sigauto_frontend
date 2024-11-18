473edfae5167592ae7d173bf997efcb66be52461
axvg <axvg@users.noreply.github.com>
2024-11-18 00:39:51 -0500

    update import

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RegisterFormComponent } from "../../components/register-form/register-form.component";
+import { RegisterFormComponent } from "@components/register-form/register-form.component";
 
 @Component({
   selector: 'app-register',
