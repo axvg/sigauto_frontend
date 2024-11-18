5c64572ce519331f6b4c47b120a7a837ccda6e65
axvg <axvg@users.noreply.github.com>
2024-11-18 00:35:46 -0500

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from '@pages/register/register.component';
-import { AppComponent } from './app.component';
+import { HomeComponent } from '@pages/home/home.component';
 
 export const routes: Routes = [
-    // { path: '', component: AppComponent },
+    { path: '', component: HomeComponent },
     { path: 'register', component: RegisterComponent }
 ];
