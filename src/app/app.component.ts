cc93b5e2fd181ba47a0ab97b81ea1e1037aaa792
axvg <axvg@users.noreply.github.com>
2024-11-20 18:34:24 -0500

    add header to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '@components/footer/footer.component';
+import { HeaderComponent } from '@components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
