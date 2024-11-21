6512988d66884914794a84c2787df7535d348302
axvg <axvg@users.noreply.github.com>
2024-11-20 22:43:08 -0500

    add service to form

--- src/app/components/vehicle-registration/vehicle-registration.component.ts
+++ src/app/components/vehicle-registration/vehicle-registration.component.ts
@@ -1,34 +1,47 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import {  FormControl, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common'; 
 import { ReactiveFormsModule } from '@angular/forms';
+import { RegisterVehicleModel } from '@models/register-vehicle.model';
+import { TokenService } from '@services/token.service';
+import { RegisterVehicleService } from '@services/register-vehicle.service';
 
 @Component({
   selector: 'app-vehicle-registration',
-  standalone: true, // Componente independiente
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule], 
   templateUrl: './vehicle-registration.component.html',
   styleUrls: ['./vehicle-registration.component.scss'],
-  imports: [CommonModule, ReactiveFormsModule] 
 })
 export class VehicleRegistrationComponent {
-  vehicleForm: FormGroup;
+  tokenService = inject(TokenService);
+  registerVehiclService = inject(RegisterVehicleService);
 
-  constructor(private fb: FormBuilder) {
-    this.vehicleForm = this.fb.group({
-      placa: ['', Validators.required],
-      marca: ['', Validators.required],
-      modelo: ['', Validators.required],
-      fecha: ['', Validators.required],
-      comentarios: ['']
-    });
-  }
+  vehicleForm = new FormGroup({
+    placa: new FormControl('', Validators.required),
+    marca: new FormControl('', Validators.required),
+    modelo: new FormControl('', Validators.required),
+    year: new FormControl(0, Validators.required),
+    // comentarios: new FormControl('')
+  });
 
   onSubmit() {
-    if (this.vehicleForm.valid) {
-      console.log(this.vehicleForm.value);
-    } else {
-      alert('Por favor, completa todos los campos correctamente.');
+    const idCliente = this.tokenService.getClienteId();
+
+    console.log('Formulario de registro de vehículo', this.vehicleForm.value);
+    console.log('id cliente', idCliente);
+
+    const requestBody: RegisterVehicleModel ={
+      marca: this.vehicleForm.value.marca || '',
+      modelo: this.vehicleForm.value.modelo || '',
+      year: +this.vehicleForm.value.year!,
+      placa: this.vehicleForm.value.placa || '',
+      idCliente: +idCliente
     }
+    
+    this.registerVehiclService.register(requestBody).subscribe((res) => {
+      console.log('Registro de vehículo exitoso', res);
+    })
   }
 
   onBack() {
