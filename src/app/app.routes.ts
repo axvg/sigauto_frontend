a72c888a2d3da9a22c6790e6a388538611f17c72
axvg <axvg@users.noreply.github.com>
2024-11-20 22:22:37 -0500

    add guards to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RegisterComponent } from '@pages/register/register.component';
 import { HomeComponent } from '@pages/home/home.component';
 import { VehicleRegistrationComponent } from '@components/vehicle-registration/vehicle-registration.component';
 import { InicioSesionComponent } from '@components/inicio-sesion/inicio-sesion.component';
+import { AuthGuard } from '@guards/auth.guard';
+import { AuthReverseGuard } from '@guards/auth-reverse.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'register', component: RegisterComponent },
-    {path: 'register-vehiculo',  component: VehicleRegistrationComponent },
-    {path: 'login', component: InicioSesionComponent}
+    { path: 'register', component: RegisterComponent, canActivate: [AuthReverseGuard] },
+    { path: 'register-vehiculo', component: VehicleRegistrationComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: InicioSesionComponent, canActivate: [AuthReverseGuard] }
 ];
