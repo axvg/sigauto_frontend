dd7c6ae436daf59b1f65545eb44fa2fe64eb95f3
axvg <axvg@users.noreply.github.com>
2024-11-20 22:22:26 -0500

    fix token

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -4,21 +4,44 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TokenService {
-  getDecodedToken(token: string): any {
-    return atob(token.split('.')[1])
-  }
-
-  getEmail(): string | null {
+  private getDecodedToken(): any | null {
     const token = localStorage.getItem('token');
     if (token) {
-      const decodedTokenString = this.getDecodedToken(token);
-      const decodedTokenJson = JSON.parse(decodedTokenString);
-      const { sub, iat, exp } = decodedTokenJson;
-      if (sub) {
-        return sub;
-      }
-      return 'user@mail.com'
+      const decodedTokenString = atob(token.split('.')[1]);
+      return JSON.parse(decodedTokenString);
     }
     return null;
+  }
+
+  getEmail(): string {
+    const decodedToken = this.getDecodedToken();
+    if (decodedToken && decodedToken.email) {
+      return decodedToken.email;
+    }
+    return 'user@mail.com';
+  }
+
+  getNames(): string {
+    const decodedToken = this.getDecodedToken();
+    if (decodedToken && decodedToken.names) {
+      return decodedToken.names;
+    }
+    return 'names';
+  }
+
+  getPersonaId(): string {
+    const decodedToken = this.getDecodedToken();
+    if (decodedToken && decodedToken.personaId) {
+      return decodedToken.personaId;
+    }
+    return 'personaId';
+  }
+
+  getClienteId(): string {
+    const decodedToken = this.getDecodedToken();
+    if (decodedToken && decodedToken.clienteId) {
+      return decodedToken.clienteId;
+    }
+    return 'clienteId';
   }
 }
