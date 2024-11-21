ce22d0611beb7fec7849339ece4845096e4af609
axvg <axvg@users.noreply.github.com>
2024-11-20 19:19:32 -0500

    update auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { environment } from '@environments/environment';
+import { TokenResponse } from '@models/token-response';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   http = inject(HttpClient);
+  router = inject(Router);
 
   login(email: string, password: string) {
     console.log('login service')
-    return this.http.post(`${environment.url}/api/auth/login`, { email, password });
+    return this.http.post<TokenResponse>(`${environment.url}/api/auth/login`, { email, password }).pipe(
+      tap((res) => {
+        localStorage.setItem('token', res?.token);
+      })
+    )
   }
 
   isLoggedIn(): boolean {
@@ -19,5 +27,6 @@ export class AuthService {
 
   logout() {
     localStorage.removeItem('token');
+    this.router.navigate(['/']);
   }
 }
