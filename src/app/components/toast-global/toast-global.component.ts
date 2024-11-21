aedf103935704944d9170f579de746a1e1f6f590
axvg <azvegab@gmail.com>
2024-11-21 17:38:00 -0500

    fix global

--- src/app/components/toast-global/toast-global.component.ts
+++ src/app/components/toast-global/toast-global.component.ts
@@ -12,21 +12,4 @@ import { ToastService } from '@services/toast.service';
   styleUrl: './toast-global.component.scss'
 })
 export class ToastGlobalComponent {
-	toastService = inject(ToastService);
-
-	showStandard(template: TemplateRef<any>) {
-		this.toastService.show({ template });
-	}
-
-	showSuccess(template: TemplateRef<any>) {
-		this.toastService.show({ template, classname: 'bg-success text-light', delay: 10000 });
-	}
-
-	showDanger(template: TemplateRef<any>) {
-		this.toastService.show({ template, classname: 'bg-danger text-light', delay: 15000 });
-	}
-
-	ngOnDestroy(): void {
-		this.toastService.clear();
-	}
 }
