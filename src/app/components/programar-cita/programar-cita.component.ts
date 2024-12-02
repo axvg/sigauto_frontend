48ddc277fe1cc9041bb9d1a20011467a576873c1
axvg <axvg@users.noreply.github.com>
2024-12-02 03:16:18 -0500

    update form

--- src/app/components/programar-cita/programar-cita.component.ts
+++ src/app/components/programar-cita/programar-cita.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TiposServicioService } from '@services/tipos-servicio.service';
+import { VehicleService } from '@services/vehicle.service';
+import { CitasService } from '@services/citas.service';
+import { TokenService } from '@services/token.service';
+import { ToastService } from '@services/toast.service';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-programar-cita',
@@ -11,31 +17,58 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrls: ['./programar-cita.component.scss']
 })
 export class ProgramarCitaComponent {
-  appointmentForm: FormGroup; // Define la propiedad 'appointmentForm'
+  form = new FormGroup({
+    vehiculo: new FormControl('', Validators.required),
+    fecha: new FormControl('', Validators.required),
+    servicio: new FormControl('', Validators.required),
+  })
 
-  constructor(private fb: FormBuilder) {
-    // Inicializa el formulario con los campos y validaciones
-    this.appointmentForm = this.fb.group({
-      nombre: ['', Validators.required],
-      telefono: ['', [Validators.required, Validators.pattern('[0-9]+')]],
-      email: ['', [Validators.required, Validators.email]],
-      vehiculo: ['', Validators.required],
-      servicio: ['', Validators.required],
-      fechaHora: ['', Validators.required]
-    });
-  }
+  vehicleService = inject(VehicleService);
+  tiposServicioService = inject(TiposServicioService);
+  citasService = inject(CitasService);
+  tokenService = inject(TokenService);
+  toastService = inject(ToastService)
+
+  vehicles$ = this.vehicleService.getVehicles();
+  servicios = this.tiposServicioService.getServicios();
 
   onSubmit() {
-    if (this.appointmentForm.valid) {
-      console.log(this.appointmentForm.value);
-      alert('Cita reservada exitosamente');
+    console.log(this.form.value);
+    if (this.form.valid) {
+      const { vehiculo, fecha, servicio } = this.form.value;
+      const idCliente = this.tokenService.getClienteId();
+
+      const cita = {
+        idCliente: +idCliente,
+        idVehiculo: Number(vehiculo || '1'),
+        idTipoServicio: Number(servicio || '1'),
+        fecha: fecha?.toString() || '',
+        estado: true
+      };
+
+      this.citasService.register(cita).pipe(
+        catchError((err) => {
+          this.toastService.show('Error al registrar la cita', 'danger');
+          return throwError(err);
+        })
+      ).subscribe((res) => {
+        console.log('Cita registrada', res);
+        this.toastService.show('Cita registrada correctamente', 'success');
+      })
+
     } else {
-      alert('Por favor, completa todos los campos correctamente.');
+      this.toastService.show('Por favor, complete los campos requeridos', 'danger');
     }
   }
 
   onBack() {
     alert('Regresando...');
+  }
+
+  ngOnInit(){
+    this.vehicleService.getVehicles().subscribe((vehicles) => {
+      console.log('Vehicles', vehicles);
+    })
   }
 }
 
